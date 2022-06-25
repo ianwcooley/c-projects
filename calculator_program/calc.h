aa6bbfeb3a79d9edc99a002d29c71e27b4afd40b
ianwcooley <ianwcooley@gmail.com>
2022-06-24 23:14:57 -0400

    did a few more exercises from Chapter 4 of The C Programming Language

--- calculator_program/calc.h
+++ calculator_program/calc.h
@@ -1,7 +1,10 @@
 #define NUMBER '0'
 void push(double);
 double pop(void);
+void clearStack(void);
 void printTop(void);
+void duplicateTop(void);
+void swapTop(void);
 int getop(char[]);
 int getch(void);
 void ungetch(int);  
--- calculator_program/stack.c
+++ calculator_program/stack.c
@@ -12,10 +12,6 @@ void push(double f) {
         val[sp++] = f;
     else
         printf("error: stack full, can't push %g\n", f);
-    
-    // printf("Pushed ");
-    // printTop();
-    // printf(" onto stack.\n");
 }
 
 /* pop: pop and return top value from stack */
@@ -28,7 +24,38 @@ double pop(void) {
     }
 }
 
+/* clearStack:  clears stack and returns stack position to 0 */
+void clearStack(void) {
+    for (int i = 0; i <= sp; i++)
+        val[i] = 0;
+    sp = 0;
+}
+
 /* printTop: prints top element of stack without popping */
 void printTop(void) {
     printf("%g", val[sp - 1]);
 }
+
+/* duplicateTop: duplicates top element of stack 
+and increments stack position */
+void duplicateTop(void) {
+    if (sp < MAXVAL) {
+        val[sp] = val[sp - 1];
+        // When I tried to increment sp in the line above I got
+        // a "warning: unsequenced modification and access to 'sp' [-Wunsequenced]"
+        // not sure why this is a problem.
+        sp++;
+    } else {
+        printf("error: stack full, can't duplicate top element.\n");
+    }
+}
+
+/* swapTop: swaps top two elements of stack */
+void swapTop(void) {
+    double tempVal = val[sp - 1];
+    val[sp-1] = val[sp-2];
+    val[sp-2] = tempVal;
+}
+
+
+
